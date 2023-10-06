e2602342c32d765e482d20ad8ec19db1f905a92c
K17UN3 <130812141+K17UN3@users.noreply.github.com>
2023-10-06 16:00:41 +0900

    mark up
    中身書き換え

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Header from "../components/header" /* importは相対パス */
-import Hero from "../components/hero"
-import Footer from "../components/footer"
+import Header from "components/header" /* importは相対パス */
+import Hero from "components/hero"
+import Footer from "components/footer"
 
 export default function Home() {
   return (
