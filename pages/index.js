c56a57bd176a13907161a9b65fbecd163bd58559
K17UN3 <130812141+K17UN3@users.noreply.github.com>
2023-10-13 14:10:10 +0900

    mark up
    Layout
    index
    Appコンポーネント

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,7 @@
-import Header from "components/header" /* importは相対パス */
-import Hero from "components/hero"
-import Footer from "components/footer"
+import Hero from "@/components/hero"
 
 export default function Home() {
   return (
-    <>
-      <Header />
-
-    <main>
       <Hero />
-    </main>
-
-      <Footer />
-    </>
   )
 }
