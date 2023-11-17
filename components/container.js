67c804d1f80378525903abae6363ea4e30909395
K17UN3 <130812141+K17UN3@users.noreply.github.com>
2023-11-17 14:25:03 +0900

    mark up

--- components/container.js
+++ components/container.js
@@ -2,8 +2,9 @@ import styles from 'styles/conteiner.module.css'
 
 export default function Conteiner({ children, large = false }) {
     return (
-        <div className={large ? styles.large : styles.default}>
-            {children}
+        <div className={styles.stack}>
+            <h3 className={styles.heading}>Contact</h3>
+            <address>cube@web.mail.address</address>
         </div>
     )
 }
