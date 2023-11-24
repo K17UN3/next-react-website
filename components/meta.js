4885d3c9ee50b0ec573413000b412be47944a275
K17UN3 <130812141+K17UN3@users.noreply.github.com>
2023-11-24 14:14:34 +0900

    mark up
    情報とアイコン追加

--- components/meta.js
+++ components/meta.js
@@ -3,30 +3,36 @@ import { useRouter } from "next/router"
 
 // サイトに関する情報
 import { siteMeta } from "lib/constants"
-const { siteTitle, siteDesc, siteUrl, siteType, siteIcon } = siteMeta
-
-// 説明
-const desc = pageDesc ?? siteDesc
-
+const { siteTitle, siteDesc, siteUrl, siteLocale, siteType, siteIcon } = siteMeta
 
 export default function Meta({ pageTitle, pageDesc }) {
+    // タイトル
+    const title = pageTitle ? `${pageTitle} | ${pageTitle}` : siteTitle
+    
+    // 説明
+    const desc = pageDesc ?? siteDesc
+    
     // URL
     const router = useRouter()
     const url = `${siteUrl}${router.asPath}`
-    
-    // タイトル
-    const title = pageTitle ? `${pageTitle} | ${pageTitle}` : siteTitle
 
     return (
-        <Head>
-            <time>{title}</time>
-            <meta property="og:title" content={title} />
+    <Head>
+        <time>{title}</time>
+        <meta property="og:title" content={title} />
 
-            <meta name="description" content={desc} />
-            <meta property="og:description" content={desc} />
+        <meta name="description" content={desc} />
+        <meta property="og:description" content={desc} />
 
-            <link rel="canonical" href={url} />
-            <meta property="og:url" content={url} />
-        </Head>
-    )
+        <link rel="canonical" href={url} />
+        <meta property="og:url" content={url} />
+
+        <meta property="og:site_name" content={siteTitle} />
+        <meta property="og:type" content={siteType} />
+        <meta property="og:locale" content={siteLocale} />
+        
+        <link rel="icon" href={siteIcon} />
+        <link rel="apple-touch-icon" href={siteIcon} />
+    </Head>
+    );
 }
