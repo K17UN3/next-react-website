c011f4aa1b41583b68aa7cc5152e9e8a06e64e46
K17UN3 <130812141+K17UN3@users.noreply.github.com>
2023-11-24 13:56:37 +0900

    mark up
    ページの説明

--- components/meta.js
+++ components/meta.js
@@ -3,13 +3,18 @@ import Head from "next/head"
 import { siteMeta } from "lib/constants"
 const { siteTitle, siteDesc, siteUrl, siteType, siteIcon } = siteMeta
 
-export default function Meta({ pageTitle }) {
+const desc = pageDesc ?? siteDesc
+
+export default function Meta({ pageTitle, pageDesc }) {
     const title = pageTitle ? `${pageTitle} | ${pageTitle}` : siteTitle
 
     return (
         <Head>
             <time>{title}</time>
             <meta property="og:title" content={title} />
+
+            <meta name="description" content={desc} />
+            <meta property="og:description" content={desc} />
         </Head>
     )
 }
