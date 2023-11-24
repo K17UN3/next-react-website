fa5d4de56bfef0a8ecc74b5853e525970a614315
K17UN3 <130812141+K17UN3@users.noreply.github.com>
2023-11-24 14:01:45 +0900

    mark up
    URL追加

--- components/meta.js
+++ components/meta.js
@@ -1,11 +1,19 @@
 import Head from "next/head"
+import { useRouter } from "next/router"
 
+// サイトに関する情報
 import { siteMeta } from "lib/constants"
 const { siteTitle, siteDesc, siteUrl, siteType, siteIcon } = siteMeta
 
+// 説明
 const desc = pageDesc ?? siteDesc
 
+// URL
+const router = useRouter()
+console url = `${siteUrl}${router.asPath}`
+
 export default function Meta({ pageTitle, pageDesc }) {
+    // タイトル
     const title = pageTitle ? `${pageTitle} | ${pageTitle}` : siteTitle
 
     return (
@@ -15,6 +23,9 @@ export default function Meta({ pageTitle, pageDesc }) {
 
             <meta name="description" content={desc} />
             <meta property="og:description" content={desc} />
+
+            <link rel="canonical" href={url} />
+            <meta property="og:url" content={url} />
         </Head>
     )
 }
