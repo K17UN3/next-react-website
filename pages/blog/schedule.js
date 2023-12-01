b17990d6a51256cafed5c4c9160e8543bd1e123c
K17UN3 <130812141+K17UN3@users.noreply.github.com>
2023-12-01 15:35:44 +0900

    mark up
    ブラー画像

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -8,6 +8,10 @@ import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from "components/two-colum
 import ConvertBody from 'components/convert-body'
 import PostCategories from 'components/post-categories'
 import Image from "next/image"
+import { getPlaiceholder } from 'plaiceholder'
+
+// ローカルの代替アイキャッチ画像
+import { eyecatchLocal } from "lib/constants"
 
 export default function Schedule({
     title,
@@ -39,6 +43,8 @@ export default function Schedule({
                         height={eyecatch.height}
                         sizes="(min-width: 1152px) 1152px, 100vw"
                         priorit
+                        placeholder="blur"
+                        blurDataURL={eyecatch.blurDataURL}
                     />
                 </figure>
 
@@ -64,12 +70,17 @@ export async function getStaticProps() {
 
     const description = extractText(post.content)
 
+    const eyecatch = post.eyecatch ?? eyecatchLocal
+
+    const { base64 } = await getPlaiceholder(eyecatch.url)
+    eyecatch.blurDataURL = base64
+
     return {
         props: {
             title: post.title,
             publish: post.publish,
             content: post.content,
-            eyecatch: post.eyecatch,
+            eyecatch: eyecatch,
             categories: post.categories,
             description: description,
         },
