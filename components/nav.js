113862d12ce23ccefb71c16bb9530bd976ff4868
K17UN3 <130812141+K17UN3@users.noreply.github.com>
2023-12-15 13:57:02 +0900

    mark up
    ハンバーガーメニュー
    ボタンクリックで更新

--- components/nav.js
+++ components/nav.js
@@ -1,9 +1,18 @@
+import { useState } from 'react'
 import Link from "next/link";
 import styles from 'styles/nav.module.css'
 
 export default function Nav() {
+  const [navIsOpne, setNavIsOpen] = useState(false)
+
+  const toggleNav = () => {
+    setNavIsOpen((prev) => !prev)
+  }
+
   return (
-    <nav>
+    <nav className={navIsOpne ? styles.open : styles.close}>
+      <button className={styles.btn}>MENU</button>
+
       <ul className={styles.list}>
         <li>
           <Link href="/">
